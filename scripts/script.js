9b33cfd0ac61a8417aaac1fa3f6882eb744848df
Nate <nathanielhill.92@gmail.com>
2019-01-24 22:22:13 -0600

    Add hover feature

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,20 @@
 const container = document.querySelector(".container");
+const boxes = document.querySelectorAll(".box");
+
 let grid;
 
 function createGrid() {
-    for (let i = 0; i < (256); i++) {
+    for (let i = 0; i < (16 ** 2); i++) {
         grid = document.createElement("div");
 
         grid.classList.add('box');
         container.appendChild(grid);
+
+        grid.addEventListener('mouseover', blackHover);
+
+        function blackHover(e) {
+            e.target.style.backgroundColor = 'black';
+        }
     }
 }
 
